86e5b56db8f13c90ae3838764023c1fb03c9e138
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-04 21:45:30 -0500

    completed shopping list challenge

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/* text is typed in input -> when Add Item is clicked 
+    should result in new li with text inputed in it */
+
+
+/* when 'Check' button is clicked, item is struckthrough */
+
+function addNewItem () {
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        let additionalItem = $("#shopping-list-entry").val();
+
+        let newLi = `<li><span class="shopping-item">${additionalItem}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+        </div>
+        </li>`;
+        
+     $('ul').append(newLi);
+    });
+}
+
+$(addNewItem);
+
+function checkItemOff () {
+    $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+       $(event.target).closest('li').children('span').toggleClass('shopping-item__checked');
+       });
+   }
+
+$(checkItemOff);
+
+/* when 'Delete button is clicked, item is deleted permenently */
+
+function deleteAnItem () {
+    $('.shopping-list').on('click', '.shopping-item-delete', event => {
+        $(event.currentTarget).closest('li').remove();
+    });
+}
+
+$(deleteAnItem);
